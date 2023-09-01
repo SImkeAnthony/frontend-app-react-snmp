281fd050b40e009b6aeeca0fb36fb774b5af6a20
SimkeAnthony <anthony.simke@std.heh.be>
2023-09-01 10:16:23 +0200

    modification de la req. axios (cors)

--- src/services/MachineEntityService.js
+++ src/services/MachineEntityService.js
@@ -4,14 +4,20 @@ import {configProd} from "../properties/config.prod.js";
 
 const config = process.env.REACT_APP_ENV === 'dev' ? configDev : configProd;
 export const MACHINE_CALL_START = config.apiBaseUri;
-export const RESCAN = config.apiBaseUri+"scan";
+export const RESCAN = config.apiBaseUri+"/scan";
+
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+}
 
 class MachineEntityService {
     static getMachineEntities(){
-        return axios.get(MACHINE_CALL_START);
+        return axios.get(MACHINE_CALL_START, {headers});
     }
     static networkScan(){
-        return axios.get(RESCAN);
+        return axios.get(RESCAN,{headers});
     }
 }
 export default MachineEntityService;
